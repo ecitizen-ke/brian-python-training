83f80f742befecb22c5c19eb585a914a7ff5b89b
brian kipkosgei <brianktoo1998@gmail.com>
2024-05-21 12:10:37 +0300

    added mod_app

---
+++ flask-calculator/mod_app/routes.py
@@ -0,0 +1,10 @@
+from . import modulo_bp
+from flask import current_app as app, request, jsonify
+from .mod import mod
+
+@modulo_bp.route('/mod', methods=['GET'])
+def add_route():
+    num1 = int(request.args.get('num1', 0))
+    num2 = int(request.args.get('num2', 0))
+    result = mod(num1, num2)
+    return jsonify({'result': result})
