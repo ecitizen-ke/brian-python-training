79a87039ca15f52b619f0a58f90a25724a9a083a
brian kipkosgei <brianktoo1998@gmail.com>
2024-05-26 16:04:26 +0300

    added whitespace to mod_app

--- flask-calculator/mod_app/routes.py
+++ flask-calculator/mod_app/routes.py
@@ -2,6 +2,7 @@ from . import modulo_bp
 from flask import current_app as app, request, jsonify
 from .mod import mod
 
+
 @modulo_bp.route('/mod', methods=['GET'])
 def add_route():
     num1 = int(request.args.get('num1', 0))
