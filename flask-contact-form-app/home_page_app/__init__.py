61f67121c88788cec0b13854c5b7756bf3f5581b
brian kipkosgei <brianktoo1998@gmail.com>
2024-05-26 13:30:29 +0300

    added home_page_app

---
+++ flask-contact-form-app/home_page_app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, render_template, redirect
+
+home_bp = Blueprint('home_bp', __name__, template_folder='templates')
+
+
+@home_bp.route('/')
+def index():
+    return render_template('index.html')
+
+
+@home_bp.route('/hello')
+def hello():
+    return 'hello world'
+
+
+@home_bp.route('/hello/<name>')
+def hello_name(name):
+    return f'hello{name}'
