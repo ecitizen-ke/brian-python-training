8056c6b7066cd9ef4a1eb22d59c6ab2369eccb5b
Brian Kipkosgei <brianktoo1998@gmail.com>
2025-01-30 00:13:15 +0300

    add class Person and User

--- python-oop/app.py
+++ python-oop/app.py
@@ -20,6 +20,33 @@ print(dog2.breed)
 
 dog1.bark()
 
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def greet(self):
+        print(f"hello, my name is {self.name} and i am {self.age} years old")    
+person1 =Person("ian", 30)    
+person1.greet()
+person2 = Person("Bob", 40)
+person2.greet()
+
+class User:
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+    def say_hi_to_user(self, user):
+        self.user = user    
+        print(f"sending message to {user.username}: Hi {user.username}, it's {self.username}")
+user1 = User("Tom", "tom@gmail.com", "123")   
+user2 = User("caleb", "caleb@gmail.com", "1334") 
+user1.say_hi_to_user(user2)    
+
+# update values of a user
+print(user1.email)
+user1.email = "xxxx@gmail.com"
+print(user1.email)
 
 
         
