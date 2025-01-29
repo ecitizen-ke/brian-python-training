336b3f5321acaebd56c4970f8e574656d233e9a0
Brian Kipkosgei <brianktoo1998@gmail.com>
2025-01-30 01:14:15 +0300

    add get and set methods

--- python-oop/app.py
+++ python-oop/app.py
@@ -1,58 +1,43 @@
-class Dog:
-    def __init__(self,name, breed, owner):
-        self.name= name
-        self.breed = breed
-        self.owner = owner
-    def bark (self):
-        print ("whoof")    
+from datetime import datetime  # Import datetime module
 
-class Owner:
-    def __init__(self, name, address, contact_number):
-        self.name = name
-        self.address= address
-        self.contact_number = contact_number    
-owner1 =  Owner("brian", "kapsabet", 9876556789)   
-owner2 = Owner("kelvin", "eldoret", 12345)    
-dog1 = Dog("sibwor", "white", owner1)
-dog2 = Dog("simba", "black", owner2)
-print(dog1.owner.name)   
-print(dog2.breed)  
-
-dog1.bark()
-
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-    def greet(self):
-        print(f"hello, my name is {self.name} and i am {self.age} years old")    
-person1 =Person("ian", 30)    
-person1.greet()
-person2 = Person("Bob", 40)
-person2.greet()
 
 class User:
     def __init__(self, username, email, password):
         self.username = username
-        self.email = email
+        self._email = email  # Private email field
         self.password = password
+
     def say_hi_to_user(self, user):
-        self.user = user    
-        print(f"sending message to {user.username}: Hi {user.username}, it's {self.username}")
-user1 = User("Tom", "tom@gmail.com", "123")   
-user2 = User("caleb", "caleb@gmail.com", "1334") 
-user1.say_hi_to_user(user2)    
+        print(f"sending message to {user.username}: Hi {
+        user.username}, it's {self.username}")
 
-# update values of a user
-print(user1.email)
-user1.email = "xxxx@gmail.com"
-print(user1.email)
+    # Method to access the email field
+    def get_email(self):
+        print(f"Email accessed at {datetime.now()}")
+        return self._email
 
-
-        
-
-
-
+    # Method to modify the email field
+    def set_email(self, new_email):
+        if "@" in new_email:
+            self._email = new_email  # Set to the private _email field
+        else:
+            print("Invalid email format!")
 
 
+# Creating two User objects
+user1 = User("Tom", "Tom@gmail.com", "123")
+user2 = User("caleb", "caleb@gmail.com", "1334")
 
+# Sending a message to user2 from user1
+user1.say_hi_to_user(user2)
+
+# Accessing email via getter method
+print(user1.get_email())  # Prints email and the time it was accessed
+
+# Trying to set an invalid email
+user1.set_email("tyuioty.com")  # Invalid email
+print(user1.get_email())  # Email won't be changed, still the original one
+
+# Setting a valid email
+user1.set_email("newemail@example.com")
+print(user1.get_email())  # Email should be updated
